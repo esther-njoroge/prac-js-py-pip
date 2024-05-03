5c5e3cac95757d6c1034aabf92fbbc12bc1650d3
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-05-03 06:37:23 +0300

    add changes

--- prac.py
+++ prac.py
@@ -5,17 +5,17 @@ def add_user(user_id, preferred_categories):
     user_preferences[user_id] = preferred_categories
     
 def get_recommendations(user_id):
-    preferred_categories = user_preferences.get(user_id, [])
+    preferred_categories = user_preferences.get(user_id, [ ])
+    print('preferred item',preferred_categories)
     if not preferred_categories:
         return []
     
     recommendations = []
     for category in preferred_categories:
         recommendations.append(f"Top item in {category}")
-        recommendations.append(f"Popular item in {category}")
     return recommendations
 # Exa
 add_user("user01", ["appliances", "clothes"])
 add_user("user02", ["groceries"])
-print(f"Recommendations for user1: {get_recommendations('user01')}")
-print(f"Recommendations for user2: {get_recommendations('user02')}")
+print(f"Recommendations for user01: {get_recommendations('user01')}")
+print(f"Recommendations for user02: {get_recommendations('user02')}")
